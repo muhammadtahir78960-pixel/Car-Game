d8ea3db22801c4258dc74cd832afc15e9479e34e
muhammadtahir78960-pixel <muhammadtahir78960@gmail.com>
2026-07-16 16:47:07 +0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,19 @@
+appname="Car Game"
+appver="1.8"
+appcode="1"
+appsdk="15"
+path_pattern=""
+packagename="com.tahirht.cargame"
+theme="Theme"
+app_key=""
+app_channel=""
+developer="Tahir HT"
+description=""
+debugmode=true
+user_permission={
+  "ACCESS_NETWORK_STATE",
+  "ACCESS_WIFI_STATE",
+  "INTERNET",
+  "VIBRATE",
+  "WRITE_EXTERNAL_STORAGE"
+}
